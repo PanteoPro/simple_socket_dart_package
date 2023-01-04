b2163eebd20867f3fb0b3c3ef089234779ea0834
kn_griva <kn_griva@protek.ru>
2023-01-04 14:38:11 +0300

    remove unused libs

--- test/simple_socket_test.dart
+++ test/simple_socket_test.dart
@@ -1,16 +1,1 @@
-import 'package:simple_socket/simple_socket.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+void main() {}
